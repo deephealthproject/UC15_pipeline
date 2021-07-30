e199fff0452d19818b09bc305bb43ff0bd319df0
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-07-30 13:01:32 +0200

    Added Datagenerator to the test function

--- eddl_pipeline/src/pipeline/test.cpp
+++ eddl_pipeline/src/pipeline/test.cpp
@@ -1,7 +1,16 @@
 #include "test.hpp"
 #include <chrono>
 
+#include "data_generator.hpp"
+
 TestResults test(ecvl::DLDataset &dataset, Net *model, Arguments &args) {
+  if (args.use_dldataset)
+    return test_dataset(dataset, model, args);
+  else
+    return test_datagen(dataset, model, args);
+}
+
+TestResults test_dataset(ecvl::DLDataset &dataset, Net *model, Arguments &args) {
   // Get test split info
   dataset.SetSplit(ecvl::SplitType::test);
   const int n_te_samples = dataset.GetSplit().size();
@@ -61,6 +70,74 @@ TestResults test(ecvl::DLDataset &dataset, Net *model, Arguments &args) {
   // Free batch memory
   delete x;
   delete y;
+
+  return TestResults(loss, acc);
+}
+
+TestResults test_datagen(ecvl::DLDataset &dataset, Net *model, Arguments &args) {
+  // Get test split info
+  dataset.SetSplit(ecvl::SplitType::test);
+  const int n_te_samples = dataset.GetSplit().size();
+  const int n_te_batches = n_te_samples / args.batch_size;
+  DataGenerator datagen(&dataset, args.batch_size, args.target_shape,
+                        {static_cast<int>(dataset.classes_.size())},
+                        args.workers);
+
+  // Reset batch counter
+  dataset.ResetAllBatches();
+
+  // Reset the accumulated loss value
+  eddl::reset_loss(model);
+
+  // Auxiliary variables to store the results
+  float loss, acc;
+
+  // Validation phase
+  float load_time = 0.f;
+  float eval_time = 0.f;
+  const auto test_start = std::chrono::high_resolution_clock::now();
+  std::cout << "\nRunning Test:\n";
+  datagen.Start();
+  for (int b = 1; datagen.HasNext(); ++b) {
+    // Load data
+    const auto load_start = std::chrono::high_resolution_clock::now();
+    Tensor *x, *y;
+    bool batch_loaded = datagen.PopBatch(x, y);
+    const auto load_end = std::chrono::high_resolution_clock::now();
+    load_time += std::chrono::duration_cast<std::chrono::microseconds>(load_end - load_start).count();
+    if (!batch_loaded) {
+      std::cerr << "Error! The batch number " << b << " failed to load!" << std::endl;
+      std::abort();
+    }
+
+    // Perform evaluation
+    const auto eval_start = std::chrono::high_resolution_clock::now();
+    eddl::eval_batch(model, {x}, {y});
+    const auto eval_end = std::chrono::high_resolution_clock::now();
+    eval_time += std::chrono::duration_cast<std::chrono::microseconds>(eval_end - eval_start).count();
+
+    // Get the current losses and metrics
+    const float curr_loss = eddl::get_losses(model)[0];
+    const float curr_acc = eddl::get_metrics(model)[0];
+
+    // Show current stats
+    std::cout << "\r";
+    std::cout << "Batch " << b << "/" << n_te_batches << ": ";
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << "Metrics[ loss=" << curr_loss << ", acc=" << curr_acc << " ]";
+    std::cout << " - Timers[ ";
+    std::cout << "avg_load_batch=" << (load_time / b)  * 1e-6 << "s";
+    std::cout << ", avg_eval_batch=" << (eval_time / b) * 1e-6 << "s ]";
+    std::cout << std::flush;
+
+    // Free memory for the next batch;
+    delete x;
+    delete y;
+  }
+  datagen.Stop();
+  const auto test_end = std::chrono::high_resolution_clock::now();
+  const float test_time = std::chrono::duration_cast<std::chrono::microseconds>(test_end - test_start).count();
+  std::cout << "\nTest time elapsed = " << test_time * 1e-6 << "s\n";
 
   return TestResults(loss, acc);
 }
--- eddl_pipeline/src/pipeline/test.hpp
+++ eddl_pipeline/src/pipeline/test.hpp
@@ -16,5 +16,13 @@ struct TestResults {
   TestResults(const float loss, float acc) : loss(loss), acc(acc) {}
 };
 
+// Main function to use. It calls test_dataset() if args.use_dldataset is true
+// else it calls test_datagen()
 TestResults test(ecvl::DLDataset &dataset, Net *model, Arguments &args);
+
+// Uses DLDataset to load the batches, no multithreading is used
+TestResults test_dataset(ecvl::DLDataset &dataset, Net *model, Arguments &args);
+
+// Using the multi threaded DataGenerator to load the batches
+TestResults test_datagen(ecvl::DLDataset &dataset, Net *model, Arguments &args);
 #endif
