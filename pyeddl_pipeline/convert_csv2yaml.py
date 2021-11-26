0401c30669f7275d9375ef0760b8fe7e4e70dcc1
chavicoski <alvaro.lopez.chilet@gmail.com>
2021-11-26 11:29:57 +0100

    Updated convert_csv2yaml.py to create datasets with the normal label. Added the creation of the Pytorch TSV

--- pyeddl_pipeline/convert_csv2yaml.py
+++ pyeddl_pipeline/convert_csv2yaml.py
@@ -29,6 +29,8 @@ def main(args):
     # Join the dataframes
     full_df = pd.concat([train_df, val_df, test_df], ignore_index=True)
 
+    labels_not_used = set(args.all_labels).difference(set(args.target_labels))
+
     # Prepare the list of labels for each sample
     def create_labels_list(row):
         sample_labels = []
@@ -36,12 +38,33 @@ def main(args):
             if row[label] == 1:
                 sample_labels.append(label)
 
+        if args.add_normal and len(sample_labels) == 0:
+            found = False
+            for label in labels_not_used:
+                if row[label] == 1:
+                    found = True
+            if not found:
+                sample_labels = ['normal']
+
         return sample_labels
 
     full_df['labels'] = full_df.apply(create_labels_list, axis=1)
 
+    if args.add_normal:
+        before_drop = len(full_df.index)
+        full_df = full_df[full_df['labels'].map(lambda x: len(x)) > 0]
+        samples_removed = before_drop - len(full_df.index)
+        print(f"Samples without a target label removed: {samples_removed}")
+        args.target_labels.append('normal')
+
     # Create the final YAML from the processed dataframe 'full_df'
     create_ecvl_yaml(full_df, args.yaml_path, args.target_labels, True)
+
+    # Store the Dataframe to a TSV for the Pytorch pipeline
+    if args.tsv_path:
+        aux_df = full_df[['subject', 'session', 'filepath', 'labels',
+                          'gender', 'age', 'split']]
+        aux_df.to_csv(args.tsv_path, sep='\t', index=False)
 
 
 if __name__ == '__main__':
@@ -70,6 +93,13 @@ if __name__ == '__main__':
                             required=True,
                             type=str)
 
+    arg_parser.add_argument("--tsv-path",
+                            help=("Path to the store the new TSV file "
+                                  "(for the Pytorch pipeline)"),
+                            required=False,
+                            default="",
+                            type=str)
+
     arg_parser.add_argument("--paths-prefix",
                             help="Prefix to add to the images paths",
                             default="",
@@ -82,5 +112,18 @@ if __name__ == '__main__':
                             nargs='+',
                             default=["infiltrates", "pneumonia", "covid19"],
                             type=str)
+
+    arg_parser.add_argument("--all-labels",
+                            help=("List with all the labels present in the "
+                                  "dataset CSV files provided"),
+                            metavar="label_name",
+                            nargs='+',
+                            default=["infiltrates", "pneumonia", "covid19"],
+                            type=str)
+
+    arg_parser.add_argument("--add-normal",
+                            help=("The samples without any label set to 1 are"
+                                  " labeled as 'normal'"),
+                            action="store_true")
 
     main(arg_parser.parse_args())
